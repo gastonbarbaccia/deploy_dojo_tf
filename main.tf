31c1b1ad02d9da580d9fcbaaa2a5bfe3f4094d85
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-08-13 01:53:20 -0300

    ok

--- main.tf
+++ main.tf
@@ -44,14 +44,14 @@ resource "aws_instance" "ec2" {
   associate_public_ip_address = true
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y && \
-              apt-get upgrade -y && \
-              apt-get install -y docker.io docker-compose git && \
-              git clone https://github.com/DefectDojo/django-DefectDojo.git && \
-              cd django-DefectDojo && \
-              docker-compose up -d
-  EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get upgrade -y
+    apt-get install -y docker.io docker-compose git
+    git clone https://github.com/DefectDojo/django-DefectDojo.git
+    cd django-DefectDojo
+    docker-compose up -d
+EOF
 
   tags = {
     Name = var.instance_name
