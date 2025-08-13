7310bf10848b6a7f8b334cd9b1f72f89aa6db7b4
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-08-13 02:10:43 -0300

    ok

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y docker.io docker-compose git
+
+systemctl enable docker
+systemctl start docker
+
 cd /home/ubuntu
-git clone https://github.com/DefectDojo/django-DefectDojo.git
+
+# Clonar como usuario ubuntu para evitar problemas de permisos
+sudo -u ubuntu git clone https://github.com/DefectDojo/django-DefectDojo.git
+
 cd django-DefectDojo
-docker-compose up -d
+
+# Ejecutar docker-compose como usuario ubuntu
+sudo -u ubuntu docker-compose up -d
