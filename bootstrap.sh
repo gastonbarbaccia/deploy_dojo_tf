c43f0061263b1d2c57dd95adaa3016dbbe5455a2
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-08-13 02:23:20 -0300

    ok

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,4 +16,4 @@ sudo -u ubuntu git clone https://github.com/DefectDojo/django-DefectDojo.git
 cd django-DefectDojo
 
 # Ejecutar docker-compose como usuario ubuntu
-sudo docker-compose up -d
+docker-compose up -d
