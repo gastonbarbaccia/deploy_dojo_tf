20351c768ab5dac97b0ca09a068678c9d3f8c2d3
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-08-13 02:05:48 -0300

    ok

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y docker.io docker-compose git
+cd /home/ubuntu
 git clone https://github.com/DefectDojo/django-DefectDojo.git
 cd django-DefectDojo
 docker-compose up -d
