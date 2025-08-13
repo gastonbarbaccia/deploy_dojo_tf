97c31c42368af2816cf8107d130b2b7bf237edd3
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-08-13 01:21:47 -0300

    ok

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 variable "aws_region" {}
 variable "instance_name" {}
 variable "instance_type" {}
